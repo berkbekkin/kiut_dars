eedd2563a2ef8fd0ce6e2c5a15487e3fbf7a8540
BEK <bekkiberkbekki@gmail.com>
2025-10-15 16:22:16 +0500

    Rasm_namuna_uchun

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+import Rasm from './IMG_2553.PNG'
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Salom Bekzod!!!</h1>
-      <h1>Ishladi</h1>
+
+
+        <img src={Rasm} width={800} alt=""/>
+
+
+
     </div>
   );
 }
